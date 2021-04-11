316a3c19bb96677bf57146e1d94f6dc8bf95f564
Harrisonkamau <kamauharrison87@gmail.com>
2021-04-11 17:01:56 +0300

    Commence Dino-human objects comparison

---
+++ objects/dinosaur.js
@@ -0,0 +1,12 @@
+class Dinosaur {
+  constructor({ species, weight, height, diet, when, where, fact }) {
+    this.species = species;
+    this.weight =  weight;
+    this.height = height;
+    this.diet = diet;
+    this.fact = fact;
+    this.when = when;
+    this.where = where;
+  }
+}
+
---
+++ objects/user.js
@@ -0,0 +1,10 @@
+class User {
+  constructor({ name, weight, height, diet }) {
+    this.name = name;
+    this.weight = weight;
+    this.height = height;
+    this.diet = diet;
+  }
+}
+
+export default User;
